eabeb78bf31d68dae7539de6f4fee4aa233b92d7
Kovshun-Olena <kovshun.elena.1973@gmail.com>
2025-02-03 17:08:14 +0200

    4-HW

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
+const form = document.querySelector(".login-form");
 
+ form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const { email, password } = form.elements;
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    if (!emailValue || !passwordValue) {
+      alert("All form fields must be filled in");
+      return;
+    }
+
+    console.log({ email: emailValue, password: passwordValue });
+    form.reset();
+  });
